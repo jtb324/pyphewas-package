94372df51f92c539b1b683958ad0256ec73921f6
J.T. Baker <james.baker@vanderbilt.edu>
2025-12-19 22:22:03 -0500

    fix(run_PheWAS.py): - jtb324 - 12/19/25 - 10:22pm --- On branch firth Changes to be committed: 	modified:   pyproject.toml 	modified:   src/pyphewas/run_PheWAS.py 	modified:   tests/integration_tests.py
    fixed a bug where firth was being used for linear & logistic regression:
    ---
    The code wasn't checking to see if the model being used originally was
    logistic before running the FirthLogisticModel. Firth is not made for
    continuous variables so this was wrong behavior. There is a check now
    that makes sure the regression is logistic before running firth

--- tests/integration_tests.py
+++ tests/integration_tests.py
@@ -20,6 +20,16 @@ LINEAR_COVARIATES = (
     INPUTS_DIR / "covariates_file_10000_samples_dosage_predictor_30_phecodes.txt"
 )
 
+PERFECT_SEP_COUNTS = (
+    INPUTS_DIR
+    / "phecodes_file_10000_samples_binary_predictor_30_phecodes_perfect_sep.txt"
+)
+
+PERFECT_SEP_COVARIATES = (
+    INPUTS_DIR
+    / "covariates_file_10000_samples_binary_predictor_30_phecodes_perfect_sep.txt"
+)
+
 # Path to the main script
 SCRIPT_PATH = Path("src/pyphewas/run_PheWAS.py")
 
@@ -92,6 +102,49 @@ def test_linear_regression(tmp_path):
         "None",
         "--model",
         "linear",
+        "--output",
+        str(output_file),
+        "--cpus",
+        "2",
+    ]
+
+    # Run the command
+    result = subprocess.run(cmd, capture_output=True, text=True)
+
+    # Check that the command ran successfully
+    assert result.returncode == 0, f"Command failed with stderr: {result.stderr}"
+
+    # Check that the output file was created
+    assert output_file.exists()
+
+    assert output_file.stat().st_size > 0
+
+
+def test_firth_regression(tmp_path) -> None:
+    output_file = tmp_path / "perfect_separation_test.txt"
+
+    cmd = [
+        sys.executable,
+        str(SCRIPT_PATH),
+        "--counts",
+        str(PERFECT_SEP_COUNTS),
+        "--covariate-file",
+        str(PERFECT_SEP_COVARIATES),
+        "--min-phecode-count",
+        "2",
+        "--min-case-count",
+        "20",
+        "--covariate-list",
+        "age",
+        "sex",
+        "--status-col",
+        "predictor",
+        "--sample-col",
+        "id",
+        "--phecode-version",
+        "None",
+        "--model",
+        "logistic",
         "--output",
         str(output_file),
         "--cpus",
